4dfae9d90cee05df9997c7627fef81d5999f8be5
Muhmed Muhmed <75679133+HAMMADIFTW@users.noreply.github.com>
2023-10-25 23:34:51 +1000

    Update forms.py
    Wrong Code commited, will fix

--- projectfile/website/forms.py
+++ projectfile/website/forms.py
@@ -11,30 +11,4 @@ class LoginForm(FlaskForm):
     password=PasswordField("Password", validators=[InputRequired('Enter password')])
     submit = SubmitField("Login")
 
- # this is the registration form
-class RegisterForm(FlaskForm):
-    firstname=StringField("First Name", validators=[InputRequired()])
-    lastname=StringField("Last Name", validators=[InputRequired()])
-    email = StringField("Email Address", validators=[Email()])
-    password=PasswordField("Password", validators=[InputRequired('Enter a password')])
-    phone=StringField("Phone", validators=[InputRequired()])
-    submit = SubmitField("Register")
-
-class CommentForm(FlaskForm):
-    text = TextAreaField('Comment', validators=[InputRequired()])
-    submit = SubmitField('Create')
-
-class EventForm(FlaskForm):
-    event_name = StringField("Event Name", validators=[InputRequired()])
-    location = StringField("Location", validators=[InputRequired()])
-    price = StringField("Price", validators=[InputRequired()])
-    date = StringField("Date", validators=[InputRequired()])
-    time = StringField("Time", validators=[InputRequired()])
-    performer = StringField("Performer", validators=[InputRequired()])
-    image = FileField('Destination Image', validators=[
-    FileRequired(message='Image cannot be empty'),
-    FileAllowed(ALLOWED_FILE, message='Only supports PNG, JPG, png, jpg')])
-    event_category = StringField("Event category", validators=[InputRequired()])
-    event_type = StringField("Event type", validators=[InputRequired()])
-    description = StringField("Description", validators=[InputRequired()])
-    comment_id = StringField("Comment", validators=[InputRequired()])
+ 
