7fb6e540be811fa89d60ca12315ca65802c4de32
Muhmed Muhmed <75679133+HAMMADIFTW@users.noreply.github.com>
2023-10-25 23:30:25 +1000

    Update forms.py

--- projectfile/website/forms.py
+++ projectfile/website/forms.py
@@ -1,21 +1,40 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import TextAreaField,SubmitField, StringField, PasswordField
-from wtforms.validators import InputRequired, Length, Email, EqualTo
+from wtforms.validators import InputRequired, Length, Email
+from flask_wtf.file import FileRequired, FileField, FileAllowed
+
+ALLOWED_FILE = {'PNG', 'JPG', 'JPEG', 'png', 'jpg', 'jpeg'}
 
 #creates the login information
 class LoginForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired('Enter user name')])
-    password=PasswordField("Password", validators=[InputRequired('Enter user password')])
+    email=StringField("Email address", validators=[InputRequired('Enter your email address')])
+    password=PasswordField("Password", validators=[InputRequired('Enter password')])
     submit = SubmitField("Login")
 
  # this is the registration form
 class RegisterForm(FlaskForm):
-    user_name=StringField("User Name", validators=[InputRequired()])
-    email_id = StringField("Email Address", validators=[Email("Please enter a valid email")])
-    #linking two fields - password should be equal to data entered in confirm
-    password=PasswordField("Password", validators=[InputRequired(),
-                  EqualTo('confirm', message="Passwords should match")])
-    confirm = PasswordField("Confirm Password")
-
-    #submit button
+    firstname=StringField("First Name", validators=[InputRequired()])
+    lastname=StringField("Last Name", validators=[InputRequired()])
+    email = StringField("Email Address", validators=[Email()])
+    password=PasswordField("Password", validators=[InputRequired('Enter a password')])
+    phone=StringField("Phone", validators=[InputRequired()])
     submit = SubmitField("Register")
+
+class CommentForm(FlaskForm):
+    text = TextAreaField('Comment', validators=[InputRequired()])
+    submit = SubmitField('Create')
+
+class EventForm(FlaskForm):
+    event_name = StringField("Event Name", validators=[InputRequired()])
+    location = StringField("Location", validators=[InputRequired()])
+    price = StringField("Price", validators=[InputRequired()])
+    date = StringField("Date", validators=[InputRequired()])
+    time = StringField("Time", validators=[InputRequired()])
+    performer = StringField("Performer", validators=[InputRequired()])
+    image = FileField('Destination Image', validators=[
+    FileRequired(message='Image cannot be empty'),
+    FileAllowed(ALLOWED_FILE, message='Only supports PNG, JPG, png, jpg')])
+    event_category = StringField("Event category", validators=[InputRequired()])
+    event_type = StringField("Event type", validators=[InputRequired()])
+    description = StringField("Description", validators=[InputRequired()])
+    comment_id = StringField("Comment", validators=[InputRequired()])
