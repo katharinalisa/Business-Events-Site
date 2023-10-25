b2c0c7ec3fd19f70815a69e96a13658abac165e8
An Vu <ak.vu@connect.qut.edu.au>
2023-10-25 11:01:04 +1000

    Update views.py

--- projectfile/website/views.py
+++ projectfile/website/views.py
@@ -1,7 +1,29 @@
 from flask import Blueprint
+from flask import Blueprint, flash, render_template, request, url_for, redirect
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
-    return '<h1>Starter code for the assessment<h1>'
+    return render_template('index.html')
+
+@main_bp.route('/content-page.html')
+def event():
+    return render_template('content-page.html')
+
+@main_bp.route('/createevent.html')
+def createevent():
+    return render_template('createevent.html')
+
+@main_bp.route('/payment.html')
+def payment():
+    return render_template('payment.html')
+
+@main_bp.route('/booking.html')
+def bookings():
+    return render_template('booking.html')
+
+@main_bp.route('/hello.html')
+def hello():
+    return render_template('hello.html')
+
