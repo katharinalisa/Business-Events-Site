bf47cf7205e4594c7228b6a0bf5c42966d4d1c08
Muhmed Muhmed <75679133+HAMMADIFTW@users.noreply.github.com>
2023-10-26 21:51:39 +1000

    Update forms.py
    added comment and event forms

--- projectfile/website/forms.py
+++ projectfile/website/forms.py
@@ -19,3 +19,24 @@ class RegisterForm(FlaskForm):
     password=PasswordField("Password", validators=[InputRequired('Enter a password')])
     phone=StringField("Phone", validators=[InputRequired()])
     submit = SubmitField("Register")
+    
+  # this is the comment form
+class CommentForm(FlaskForm):
+    text = TextAreaField('Comment', validators=[InputRequired()])
+    submit = SubmitField('Create')
+    
+  # this is the event form
+class EventForm(FlaskForm):
+    event_name = StringField("Event Name", validators=[InputRequired()])
+    location = StringField("Location", validators=[InputRequired()])
+    price = StringField("Price", validators=[InputRequired()])
+    date = StringField("Date", validators=[InputRequired()])
+    time = StringField("Time", validators=[InputRequired()])
+    performer = StringField("Performer", validators=[InputRequired()])
+    image = FileField('Destination Image', validators=[
+    FileRequired(message='Image cannot be empty'),
+    FileAllowed(ALLOWED_FILE, message='Only supports PNG, JPG, png, jpg')])
+    event_category = StringField("Event category", validators=[InputRequired()])
+    event_type = StringField("Event type", validators=[InputRequired()])
+    description = StringField("Description", validators=[InputRequired()])
+    comment_id = StringField("Comment", validators=[InputRequired()])
