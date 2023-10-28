bdc88572a4e0d702cea4258399f75a29c4b46c16
An Vu <ak.vu@connect.qut.edu.au>
2023-10-28 13:41:46 +1000

    Update event.py

--- projectfile/website/event.py
+++ projectfile/website/event.py
@@ -1,1 +1,19 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from .models import Event, Comment
+from .forms import EventForm, CommentForm
+from . import db
+from flask import Flask, flash, redirect, render_template, request, url_for
+from datetime import datetime
+import sqlalchemy.exc
+from flask_login import LoginManager
+import os
+from werkzeug.utils import secure_filename
+from flask_login import login_required, current_user
 
+destbp = Blueprint('event', __name__)
+
+@destbp.route('/<event_id>', methods=['GET', 'POST'])
+def show(event_id):
+    event = db.session.scalar(db.select(Event).where(Event.event_id==event_id))
+    form = CommentForm()    
+    return render_template('content-page.html', event=event, form=form)
