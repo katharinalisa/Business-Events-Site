3f0ac7ba5e062a251bb59388ddf39019bebaa2d9
Muhmed Muhmed <75679133+HAMMADIFTW@users.noreply.github.com>
2023-10-29 18:11:51 +1000

    Update views.py

--- projectfile/website/views.py
+++ projectfile/website/views.py
@@ -6,22 +6,27 @@ from flask import Blueprint, flash, render_template, request, url_for, redirect
 
 main_bp = Blueprint('main', __name__)
 
+# Route for the home page (index).
 @main_bp.route('/')
 def index():
     return render_template('index.html')
-
+    
+# Route for the event content page.
 @main_bp.route('/content-page.html')
 def event():
     return render_template('content-page.html')
-
+    
+# Route for the event creation page.
 @main_bp.route('/createevent.html')
 def createevent():
     return render_template('createevent.html')
-
+    
+# Route for the payment page
 @main_bp.route('/payment.html')
 def payment():
     return render_template('payment.html')
-
+    
+# Route for the booking page.
 @main_bp.route('/booking.html')
 def bookings():
     return render_template('booking.html')
