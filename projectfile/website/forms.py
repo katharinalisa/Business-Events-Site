fa181c243d428b2ce5f26f0adc68948eeaae8d33
Muhmed Muhmed <75679133+HAMMADIFTW@users.noreply.github.com>
2023-11-03 18:17:26 +1000

    Update forms.py

--- projectfile/website/forms.py
+++ projectfile/website/forms.py
@@ -5,26 +5,25 @@ from flask_wtf.file import FileRequired, FileField, FileAllowed
 
 ALLOWED_FILE = {'PNG', 'JPG', 'JPEG', 'png', 'jpg', 'jpeg'}
 
-# Code to create the login form
+#creates the login information
 class LoginForm(FlaskForm):
     email=StringField("Email address", validators=[InputRequired('Enter your email address')])
     password=PasswordField("Password", validators=[InputRequired('Enter password')])
     submit = SubmitField("Login")
 
-  # this is the registration form
+ # this is the registration form
 class RegisterForm(FlaskForm):
     firstname=StringField("First Name", validators=[InputRequired()])
     lastname=StringField("Last Name", validators=[InputRequired()])
     email = StringField("Email Address", validators=[Email()])
-    password=PasswordField("Password", validators=[InputRequired('Enter a password')])
-    phone=StringField("Phone", validators=[InputRequired()])
+    password=PasswordField("Password", validators=[InputRequired(message='Enter a password')])
+    phone=StringField("Phone")
     submit = SubmitField("Register")
-    
-  class CommentForm(FlaskForm):
-    text = TextAreaField('Comment', validators=[InputRequired()])
+
+class CommentForm(FlaskForm):
+    text = TextAreaField('Comment')
     submit = SubmitField('Create')
-    
-  # Code to create the Event Form
+
 class EventForm(FlaskForm):
     event_name = StringField("Event Name", validators=[InputRequired()])
     street = StringField("Street", validators=[InputRequired()])
@@ -33,22 +32,24 @@ class EventForm(FlaskForm):
     state = StringField("State", validators=[InputRequired()])
     price = StringField("Price", validators=[InputRequired()])
     date = StringField("Date", validators=[InputRequired()])
-    time = StringField("Time", validators=[InputRequired()])
+    starttime = StringField("Start time", validators=[InputRequired()])
+    endtime = StringField("End time", validators=[InputRequired()])
     performer = StringField("Performer", validators=[InputRequired()])
-    image = FileField('Destination Image', validators=[
+    tickets_available = StringField("Tickets available", validators=[InputRequired()])
+    image = FileField("Image", validators=[
     FileRequired(message='Image cannot be empty'),
     FileAllowed(ALLOWED_FILE, message='Only supports PNG, JPG, png, jpg')])
     event_category = StringField("Event category", validators=[InputRequired()])
     event_type = StringField("Event type", validators=[InputRequired()])
-    description = StringField("Description", validators=[InputRequired()])
-    comment_id = StringField("Comment", validators=[InputRequired()])
+    description = TextAreaField("Description", validators=[InputRequired()])
     submit = SubmitField('Create Event')
-    # Code to create the booking form
+
 class BookingForm(FlaskForm):
-    booking_ref=StringField("First Name", validators=[InputRequired()])
-    event_name=StringField("Last Name", validators=[InputRequired()])
-    booking_datetime = StringField("Email Address", validators=[Email()])
-    password=PasswordField("Password", validators=[InputRequired(message='Enter a password')])
-    phone=StringField("Phone")
-    submit = SubmitField("Register")
-    
+    booking_ref=StringField("Booking Reference", validators=[InputRequired()])
+    event_name=StringField("Event Name", validators=[InputRequired()])
+    booking_datetime = StringField("Booking Datetime", validators=[InputRequired()])
+    price = StringField("Price", validators=[InputRequired()])
+    tickets_available = StringField("Tickets available")
+    image = StringField("Image", validators=[InputRequired()])
+    event_id = StringField("Event ID", validators=[InputRequired()])
+    submit = SubmitField("Proceed to pay")
